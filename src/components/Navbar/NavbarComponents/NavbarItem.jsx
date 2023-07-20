4c26163a799ffb9bd194684ec5012d4b29118376
abel34 <abel.dominguez@sw-argos.com>
2023-07-20 12:19:36 -0600

    add new effect to make it look like something is being selected

--- src/components/Navbar/NavbarComponents/NavbarItem.jsx
+++ src/components/Navbar/NavbarComponents/NavbarItem.jsx
@@ -1,5 +1,5 @@
 const NavbarItem = ({children, title}) => {
-  return <li className="flex items-center h-[50px] pl-3 gap-3 text-3xl hover:bg-gray-100 hover:rounded-lg hover:h-[50px] hover:ease-in duration-200 hover:cursor-pointer">
+  return <li className="flex items-center h-[50px] pl-3 gap-3 text-3xl hover:bg-gray-100 hover:rounded-lg hover:h-[50px] hover:ease-in duration-200 hover:cursor-pointer hover:scale-105">
     {children}
     <span className="text-base">{title}</span>
   </li>
