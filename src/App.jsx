84cdd566773184f588149e01eeb38e45a9216d22
abel34 <abel.dominguez@sw-argos.com>
2023-07-20 09:58:44 -0600

    specify the 3 sections of page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
 
   return (
-    <>
+    <div className="grid grid-cols-3">
      <Navbar/>
-    </>
+     <div>
+      Content
+     </div>
+     <div>
+      User
+     </div>
+    </div>
   )
 }
 
