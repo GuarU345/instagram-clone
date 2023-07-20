aee5ce2d8817eb855bfaf018793ab7e0c8455df4
abel34 <abel.dominguez@sw-argos.com>
2023-07-20 08:35:47 -0600

    add react-icons library

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+  return (
+    <div>
+        <h3>Instagram</h3>
+        <section>
+            <ul>
+                <li>Inicio</li>
+                <li>Busqueda</li>
+                <li>Explorar</li>
+                <li>Reels</li>
+                <li>Mensajes</li>
+                <li>Notificaciones</li>
+                <li>Crear</li>
+                <li>Perfil</li>
+            </ul>
+        </section>
+    </div>
+  )
+}
+
+export default Navbar
