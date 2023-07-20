8903beb546d65f2ee72add7e3d6d3497aa71eadc
abel34 <abel.dominguez@sw-argos.com>
2023-07-20 12:23:54 -0600

    add the main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import InstagramStates from "./MainComponents/InstagramStates"
+import Post from "./MainComponents/Post"
+
+const Main = () => {
+  return (
+    <div>
+        <InstagramStates/>
+        <Post/>
+    </div>
+  )
+}
+
+export default Main
