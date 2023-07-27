d9b921621ab90c67b5d94c49b25fbb0d1c76d7d4
abel34 <abel.dominguez@sw-argos.com>
2023-07-27 07:57:49 -0600

    add new services to get a posts create and like

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,6 +1,25 @@
 import axios from "axios";
 
-export const fetchCreateNewPost = async(payload) => {
-    const {data} = await axios.post("http://localhost:4000/api/v1/post",payload)
+const INSTAGRAM_URL = "http://192.168.1.30:4000";
+
+const token = localStorage.getItem("token");
+
+const api = axios.create({
+  baseURL: INSTAGRAM_URL + "/api/v1",
+  headers: { Authorization: `Bearer ${token}` },
+});
+
+export const fetchCreateNewPost = async (payload) => {
+  const { data } = await api.post(`/post`, payload);
+  return data;
+};
+
+export const fetchGetPosts = async () => {
+  const { data } = await api.get(`/post`);
+  return data;
+};
+
+export const fetchLikePost = async(idPost) => {
+    const {data} = await api.get(`/post/${idPost}/vote`)
     return data
-}
+} 
