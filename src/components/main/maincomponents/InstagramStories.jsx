b0d9a0628031fd3d155a276234292e6ef097611a
abel34 <abel.dominguez@sw-argos.com>
2023-07-27 08:02:31 -0600

    add tailwindcss classnames in a image of stories

--- src/components/main/maincomponents/InstagramStories.jsx
+++ src/components/main/maincomponents/InstagramStories.jsx
@@ -6,7 +6,7 @@ const InstagramStories = () => {
         <ul className='flex items-center gap-2 p-3 w-[530px] relative overflow-hidden'>
             {stories.map(storie => {
              return(
-              <li key={storie.userName}>
+              <li key={storie.userName} className='min-w-[64px] flex flex-col items-center gap-2'>
                 <img src={storie.image} width={50} height={50} className='border-[3px] border-indigo-500/100 rounded-full' alt={`user image of ${storie.userName}`} />
                 <span className='text-xs'>{storie.userName}</span>
               </li>
