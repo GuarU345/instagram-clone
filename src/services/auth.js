9441114786ae23392def964cd7c2ac79db4da3da
abel34 <abel.dominguez@sw-argos.com>
2023-07-27 07:58:19 -0600

    add new service to authenticate a user

---
+++ src/services/auth.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const INSTAGRAM_URL = "http://192.168.1.30:4000";
+
+export const signup = async (body) => {
+  const { data } = await axios.post(
+    `${INSTAGRAM_URL}/api/v1/auth/signup`,
+    body
+  );
+  return data;
+};
+
+export const signing = async (body) => {
+  const payload = {
+    username: body.get("username"),
+    password: body.get("password"),
+  };
+  const { data } = await axios.post(
+    `${INSTAGRAM_URL}/api/v1/auth/signin`,
+    payload
+  );
+  return data;
+};
