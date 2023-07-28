700960220ae96ad5d9e74682487db25a4f2dc364
abel34 <abel.dominguez@sw-argos.com>
2023-07-28 13:12:43 -0600

    add function to create a comment

--- src/components/main/maincomponents/posts/BodyPost.jsx
+++ src/components/main/maincomponents/posts/BodyPost.jsx
@@ -1,23 +1,50 @@
 import { BsHeart, BsChat, BsSend, BsSave } from "react-icons/bs";
 import { fetchLikePost } from "../../../../services/posts";
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import { toast } from "react-toastify";
 import { getPostsContext } from "../../../contexts/GetPostsContext";
+import { CreateNewComment } from "../../../../services/comments";
+import { AuthContext } from "../../../contexts/AuthContext";
+import CommentMain from "../comments/CommentMain";
+import { useModal } from "../../../hooks/useModal";
 
 const BodyPost = ({ post }) => {
+  const { closeModal, isOpen, openModal } = useModal();
   const [isVoted, setIsVoted] = useState(post.isVoted);
-  const {getPosts} = useContext(getPostsContext)
+  const { getPosts } = useContext(getPostsContext);
+  const { getToken } = useContext(AuthContext);
+
+  const commentRef = useRef(null);
 
   const handleLikePost = async (event) => {
     event.preventDefault();
     try {
       await fetchLikePost(post.id);
       setIsVoted(!post.isVoted);
-      await getPosts()
+      await getPosts();
     } catch (error) {
       toast.error("Algo salio mal");
     }
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const token = getToken();
+    const comment = commentRef.current.value;
+    try {
+      await CreateNewComment(post.id, comment, token);
+      document.getElementById("form-comment").reset();
+      await getPosts();
+    } catch (error) {
+      toast.error("Algo salio mal");
+    }
+  };
+
+  const openCommentsModal = () => {
+    openModal();
+    document.body.style.overflow = "hidden";
+  };
+
   return (
     <div>
       <img src={post.media[0]} alt="image of post" />
@@ -43,17 +70,20 @@ const BodyPost = ({ post }) => {
       <section className="flex flex-col p-2">
         <span className="">{post.votes} Me gusta</span>
         <span>{post.description}</span>
-        <a className="text-gray-400" href="">
+        <a className="text-gray-400" onClick={openCommentsModal}>
           Ver los {post.comments} comentarios
         </a>
-        <form>
+        <form onSubmit={handleSubmit} id="form-comment">
           <input
+            ref={commentRef}
             className="appearance-none outline-none"
             placeholder="Añade un comentario..."
+            name="comment"
           />
         </form>
       </section>
       <hr />
+      <CommentMain handleClose={closeModal} isOpen={isOpen} />
     </div>
   );
 };
