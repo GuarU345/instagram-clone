1beba3e627cfacb633742dd93932f661693f88a9
abel34 <abel.dominguez@sw-argos.com>
2023-07-28 13:12:08 -0600

    minor changes

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,15 +1,11 @@
 import InstagramStories from "./maincomponents/InstagramStories";
 import Post from "./maincomponents/posts/Post";
-import { useContext } from "react";
-import PostModal from "../modals/PostModal";
-import { openPostModalContext } from "../contexts/OpenPostModal";
+
 
 const Main = () => {
-  const { openPostModal, setOpenPostModal } = useContext(openPostModalContext);
   return (
     <div>
       <InstagramStories />
-      {openPostModal ? <PostModal /> : null}
       <Post />
     </div>
   );
