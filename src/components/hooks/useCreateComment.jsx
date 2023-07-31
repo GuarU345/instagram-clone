e443b8580af25c89bc5674db157efe3398b7fdef
abel34 <abel.dominguez@sw-argos.com>
2023-07-31 08:14:53 -0600

    create hook useCreateComment because the action is realized in differents points of application

---
+++ src/components/hooks/useCreateComment.jsx
@@ -0,0 +1,14 @@
+import { useState,useContext } from "react";
+import { CreateNewComment } from "../../services/comments";
+import { AuthContext } from "../contexts/AuthContext";
+export function useCreateComment(idPost) {
+  const { getToken } = useContext(AuthContext);
+
+  const createComment = async (comment) => {
+    const token = getToken();
+    const resp = await CreateNewComment(idPost, comment, token);
+    return resp;
+  };
+
+  return { createComment };
+}
