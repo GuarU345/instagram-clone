8229bb0fc3f024a676dd68eb4e15606232eed3fd
abel34 <abel.dominguez@sw-argos.com>
2023-08-01 08:28:30 -0600

    create a layout to change the children if is necesary

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-import OpenPostModalProvider from "../contexts/OpenPostModal";
-import Navbar from "../navbar/Navbar";
 import Main from "./Main";
-import GetPostsProvider from "../contexts/GetPostsContext";
-import { AuthContext } from "../contexts/AuthContext";
+import Layout from "./Layout";
 
 const Home = () => {
   return (
-    <OpenPostModalProvider>
-      <GetPostsProvider>
-          <div className="grid grid-cols-3 h-screen overflow-y-hidden">
-            <Navbar />
-            <div className="w-[530px] overflow-scroll">
-              <Main />
-            </div>
-            <div>User</div>
-          </div>
-      </GetPostsProvider>
-    </OpenPostModalProvider>
+    <Layout>
+      <div className="w-[530px]">
+        <Main />
+      </div>
+      <div>User</div>
+    </Layout>
   );
 };
 
