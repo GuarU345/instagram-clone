412d4e94dd9dfbc7b720ab4b0fbe9ea18c22f3e4
ruizavi <avi.ruiz@proton.me>
2023-10-06 20:52:09 -0600

    restart project with typescript

--- src/components/navbar/navbarcomponents/NavbarItem.jsx
+++
@@ -1,8 +0,0 @@
-const NavbarItem = ({children, title, ...attributes}) => {
-  return <li {...attributes} className="flex items-center h-[50px] pl-3 gap-3 text-3xl hover:bg-gray-100 hover:rounded-lg hover:h-[50px] hover:ease-in duration-200 hover:cursor-pointer hover:scale-105">
-    {children}
-    <span className="text-base">{title}</span>
-  </li>
-}
-
-export default NavbarItem
--- src/pages/Signup.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Register from '../components/user/Register'
-
-function Signup() {
-  return <Register/>
-}
-
-export default Signup
