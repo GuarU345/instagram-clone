4936548aaef798346dc931fc18eaf1e2e16f7f3d
ruizavi <avi.ruiz@proton.me>
2023-10-07 12:47:17 -0600

    Add protection for any routes using auth store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import useAuthStore from "./hooks/useAuth";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home";
 
 function App() {
-  return <Login />;
+  const { isAuth } = useAuthStore();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute isAuth={isAuth} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
